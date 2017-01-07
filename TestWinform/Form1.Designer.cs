a0cb2521224f64f2d4416a2e2e764b3ad37a7b23
Taki <mister_zheng@sina.com>
2017-01-07 23:57:18 +0800

    测试

--- TestWinform/Form1.Designer.cs
+++ TestWinform/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(229, 173);
+            this.button1.Location = new System.Drawing.Point(190, 131);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(180, 94);
             this.button1.TabIndex = 0;
